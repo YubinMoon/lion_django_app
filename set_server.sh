9222afbb8789478983944c13714a9405ea4e9a1c
terry <munu220@gmail.com>
2023-08-01 11:40:17 +0900

    MOD set_server.sh

--- set_server.sh
+++ set_server.sh
@@ -19,8 +19,8 @@ server {
 
   location / {
     proxy_pass http://127.0.0.1:8000;
-    proxy_set_header Host $Host;
-    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header Host \$Host;
+    proxy_set_header X-Real-IP \$remote_addr;
   }
 }
 EOF"
