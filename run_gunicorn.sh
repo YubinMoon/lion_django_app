9dc9aa93dd393b6aeac3970d2620e6ed21eb7a02
terry <munu220@gmail.com>
2023-08-01 11:28:55 +0900

    ADD run_gunicorn

---
+++ run_gunicorn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 실행 경로 진입
+cd /home/lion/lion_django_app/lion_app
+# activate venv
+if [ -d "venv/Scripts" ]; then
+  source /home/lion/lion_django_app/venv/Scripts/activate
+else
+  source /home/lion/lion_django_app/venv/bin/activate
+fi
+# gunicon 실행
+gunicorn lion_app.wsgi:application --config lion_app/gunicorn.conf.py
