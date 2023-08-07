e9e9726b5f0b4923890ee5d17f0141374a331253
terry <munu220@gmail.com>
2023-08-07 05:09:55 +0000

    add postgresql

--- lion_app/lion_app/settings.py
+++ lion_app/lion_app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "lion_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
