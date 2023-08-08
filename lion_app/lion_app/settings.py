787a84fcae3f721e8d359969d0d6a6104373d128
terry <munu220@gmail.com>
2023-08-08 01:56:00 +0000

    저장

--- lion_app/lion_app/settings.py
+++ lion_app/lion_app/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.getenv("DB_HOST", "localhost"),
+        "OPTIONS": {"options": "-c search_path=sample,public"},
     }
 }
 
