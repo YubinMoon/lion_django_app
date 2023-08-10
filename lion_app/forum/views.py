e417a3a4534cad618798db3ec13c985bbffcb759
terry <munu220@gmail.com>
2023-08-10 06:25:57 +0000

    tag 변경

--- lion_app/forum/views.py
+++ lion_app/forum/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
 
 from .models import Topic, Post
 from .serializer import TopicSerializer, PostSerializer
 
 
+@extend_schema(tags=["Topic"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
