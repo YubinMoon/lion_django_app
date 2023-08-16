1f4db5ceb68f2efb4f1677f63b1aa945f4d39a6e
terry <munu220@gmail.com>
2023-08-16 02:28:46 +0000

    sdfsdf

--- lion_app/lion_app/settings/base.py
+++ lion_app/lion_app/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # from common.aws import get_secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # secret = get_secret()
 
