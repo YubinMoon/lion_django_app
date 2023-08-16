bd79aef754c43b33d602624fddb97a45ed915646
terry <munu220@gmail.com>
2023-08-16 02:09:31 +0000

    settings 변경

---
+++ lion_app/lion_app/settings/prod.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "lion-lb-18904498-6a890bf84dc5.kr.lb.naverncp.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://lion-lb-18904498-6a890bf84dc5.kr.lb.naverncp.com",
+]
---
+++ lion_app/lion_app/settings/staging.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "stage-lb-18949272-bc597543c04f.kr.lb.naverncp.com",
+]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://stage-lb-18949272-bc597543c04f.kr.lb.naverncp.com/",
+# ] nginx 없음
