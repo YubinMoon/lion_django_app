7d855c5cc7f6edeeff7d3cb8a6f40af3bba4a04b
terry <munu220@gmail.com>
2023-08-18 00:21:51 +0000

    sdfs

--- lion_app/forum/tests.py
+++ lion_app/forum/tests.py
@@ -11,10 +11,11 @@ class PostTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.superuser = User.objects.create_superuser("superuser")
+        cls.topic_owner = User.objects.create_user("topic_owner")
         cls.private_topic = Topic.objects.create(
             name="private topic",
             is_private=True,
-            owner=cls.superuser,
+            owner=cls.topic_owner,
         )
         cls.public_topic = Topic.objects.create(
             name="public topic",
@@ -35,12 +36,18 @@ class PostTest(APITestCase):
                 topic=cls.private_topic,
                 author=cls.superuser,
             )
-        cls.authorized_user = User.objects.create_user("authorized")
+        cls.common_user = User.objects.create_user("common_user")
+        cls.admin_user = User.objects.create_user("admin_user")
         cls.unauthorized_user = User.objects.create_user("unauthorized")
         TopicGroupUser.objects.create(
             topic=cls.private_topic,
-            user=cls.authorized_user,
+            user=cls.common_user,
             group=TopicGroupUser.GroupChoices.COMMON,
+        )
+        TopicGroupUser.objects.create(
+            topic=cls.private_topic,
+            user=cls.admin_user,
+            group=TopicGroupUser.GroupChoices.ADMIN,
         )
 
     def test_write_permission_on_private_topic(self):
@@ -54,9 +61,26 @@ class PostTest(APITestCase):
         self.client.force_login(self.unauthorized_user)
         res = self.client.post(reverse("post-list"), data=data)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-        # when authorized user tries to write a post on topics => success. 201
-        self.client.force_login(self.authorized_user)
-        data["author"] = self.authorized_user.pk
+
+        # when common group user tries to write a post on topics => success. 201
+        self.client.force_login(self.common_user)
+        data["author"] = self.common_user.pk
+        res = self.client.post(reverse("post-list"), data=data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        data = json.loads(res.content)
+        Post.objects.get(pk=data["id"])
+
+        # when admin group user tries to write a post on topics => success. 201
+        self.client.force_login(self.admin_user)
+        data["author"] = self.admin_user.pk
+        res = self.client.post(reverse("post-list"), data=data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        data = json.loads(res.content)
+        Post.objects.get(pk=data["id"])
+
+        # when owner user tries to write a post on topics => success. 201
+        self.client.force_login(self.topic_owner)
+        data["author"] = self.topic_owner.pk
         res = self.client.post(reverse("post-list"), data=data)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         data = json.loads(res.content)
@@ -77,7 +101,7 @@ class PostTest(APITestCase):
         res = self.client.get(reverse("topic-posts", args=[self.private_topic.pk]))
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
         # for authorized user => success. 200
-        self.client.force_login(self.authorized_user)
+        self.client.force_login(self.common_user)
         res = self.client.get(reverse("topic-posts", args=[self.private_topic.pk]))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         data = json.loads(res.content)
